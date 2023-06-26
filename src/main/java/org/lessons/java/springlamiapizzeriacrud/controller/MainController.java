f90133961f104421800214775b2df79af2849de3
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-26 08:39:14 +0200

    detail controller + html page

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/MainController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/MainController.java
@@ -3,13 +3,16 @@ package org.lessons.java.springlamiapizzeriacrud.controller;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -32,7 +35,18 @@ public class MainController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer pizzaId, Model model) {
-        model.addAttribute("pizza", pizzaId);
-        return "show";
+        //.findById gestisce un oggetto di tipo Optional che gestisce anche caso in cui arrivi un Id non sistente
+        Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+        if (result.isPresent()) {
+            //passa la pizza alla view
+            model.addAttribute("pizza", result.get());
+            return "show";
+        } else {
+            //ritorno http status 404
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Pizza with id = " + pizzaId + " not found, sorry!");
+        }
+
+
     }
 }
