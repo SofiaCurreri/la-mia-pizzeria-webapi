7a523669417b98ea02e5d01fc7e1eab0239e79ff
SofiaCurreri <sofiacurreri@gmail.com>
2023-08-01 13:24:28 +0200

    getting Pizza details

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/api/PizzaRestController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -3,16 +3,16 @@ package org.lessons.java.springlamiapizzeriacrud.api;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/pizzas")
+@RequestMapping("/api/v1/pizzas")
 public class PizzaRestController {
 
     @Autowired
@@ -22,5 +22,17 @@ public class PizzaRestController {
     @GetMapping
     public List<Pizza> index() {
         return pizzaRepository.findAll();
+    }
+
+    //servizio per dettaglio singola Pizza
+    @GetMapping("/{id}")
+    public Pizza get(@PathVariable Integer id) {
+        //cerco pizza con quell' id sul db
+        Optional<Pizza> pizza = pizzaRepository.findById(id);
+        if (pizza.isPresent()) {
+            return pizza.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
